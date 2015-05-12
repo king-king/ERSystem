5e8cbe05c4ee343c336411c35387d496a6e79fd1
WQ <niuyiwangqun@sohu.com>
2015-05-12 17:42:12 +0800

    c

--- master.js
+++ master.js
@@ -1,14 +1,13 @@
 /**
  * Created by WQ on 2015/5/12.
  */
-var cd = require( "child_process " );
 
-
-var httpServer = cd.fork( "server-worker.js" );
+var child_process = require( "child_process" );
+var httpServer = child_process.fork( "server-worker.js" );
 
 httpServer.on( "exit", function () {
     setTimeout( function () {
         // 1min 后重启
-        httpServer = cd.fork( "server-worker.js" )
+        httpServer = child_process.fork( "server-worker.js" )
     }, 1000 * 60 );
 } );
