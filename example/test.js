9d1f4fc0dfa30cbcbc9aea26f840b6bcfca5e81c
WQ <niuyiwangqun@sohu.com>
2015-05-15 18:07:02 +0800

    c

--- example/test.js
+++ example/test.js
@@ -1,4 +1,13 @@
-d = new Date()
+// 将北京时间今日0点构造成utc时间
+var d0 = new Date();
+d0.setMinutes( 0 );
+d0.setSeconds( 0 );
+d0.setHours( d0.getHours() - 8 );
+
+// 将北京当前时间构造成utc时间
+var dn = new Date();
+dn.setHours( dn.getHours() - 8 );
+
 db.chuye.aggregate( [
     {
         $match : {
@@ -11,15 +20,35 @@ db.chuye.aggregate( [
             month : {$month : "$date"},
             day : {$dayOfMonth : "$date"},
             hour : {$hour : "$date"},
-            err : 1
+            err : 1,
+            date : 1
         }
     },
     {
         $match : {
-            year : d.getFullYear(),
-            month : d.getMonth() + 1,
-            day : d.getDate(),
-            err : "510 Not Extended"
+            year : {
+                $gte : d0.getFullYear(),
+                $lte : dn.getFullYear()
+            },
+            month : {
+                $gte : d0.getMonth() + 1,
+                $lte : dn.getMonth() + 1
+            },
+            day : {
+                $gte : d0.getDate(),
+                $lte : dn.getDate()
+            },
+            err : "503 Service Unavailable"
+        }
+    },
+    {
+        $group : {
+            _id : {
+                year : 1,
+                month : 1,
+                data : 1,
+                hour : 1
+            }
         }
     }
 ] )
--- mongo.js
+++ mongo.js
@@ -6,13 +6,75 @@ var MongoClient = require( 'mongodb' ).MongoClient;
 var deleteDB = require( "deleteDB" );
 
 
-var url = 'mongodb://localhost:27017/mytest';
+var url = 'mongodb://localhost:27017/errlog';
 MongoClient.connect( url, function ( err, db ) {
 
-    deleteDB.dropOneCollection( db, "test1", function ( err, result ) {
-        console.log( err ? err : result );
-        db.close();
-    } )
+    // 将北京时间今日0点构造成utc时间
+    var d0 = new Date();
+    d0.setMinutes( 0 );
+    d0.setSeconds( 0 );
+    d0.setHours( d0.getHours() - 8 );
+
+    // 将北京当前时间构造成utc时间
+    var dn = new Date();
+    dn.setHours( dn.getHours() - 8 );
+
+    var chuye = db.collection( "chuye" );
+
+
+    chuye.aggregate(
+        [
+            {
+                $match : {
+                    solve : false
+                }
+            },
+            {
+                $project : {
+                    year : {$year : "$date"},
+                    month : {$month : "$date"},
+                    day : {$dayOfMonth : "$date"},
+                    hour : {$hour : "$date"},
+                    err : 1,
+                    date : 1
+                }
+            },
+            {
+                $match : {
+                    year : {
+                        $gte : d0.getFullYear(),
+                        $lte : dn.getFullYear()
+                    },
+                    month : {
+                        $gte : d0.getMonth() + 1,
+                        $lte : dn.getMonth() + 1
+                    },
+                    day : {
+                        $gte : d0.getDate(),
+                        $lte : dn.getDate()
+                    },
+                    err : "bad request 400"
+                }
+            },
+            {
+                $group : {
+                    _id : {
+                        "year" : "$year",
+                        "month" : "$month",
+                        "data" : "$data",
+                        "hour" : "$hour"
+                    },
+                    count : {
+                        $sum : 1
+                    }
+                }
+            }
+        ],
+        function ( err, reslut ) {
+            console.log( err ? err : reslut );
+            db.close();
+        }
+    )
 
 } );
 
